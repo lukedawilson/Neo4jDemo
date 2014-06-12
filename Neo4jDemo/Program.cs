ac9f6ec222a56cf158f65039059256f230516901
lukew <luke.wilson@trayport.com>
2014-06-12 10:56:16 +0100

    Initial commit. Creates and deletes a node.

---
+++ Neo4jDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Neo4jClient;
+using Neo4jDemo.Annotations;
+
+namespace Neo4jDemo
+{
+   [UsedImplicitly]
+   class Program
+   {
+      static void Main()
+      {
+         var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+         client.Connect();
+
+         var newNode = client.Create(new {name = "Jimi Hendrix", type = "artist"});
+         Console.WriteLine(newNode.Id);
+
+         Console.ReadKey();
+
+         client.Delete(newNode, DeleteMode.NodeAndRelationships);
+      }
+   }
+}
