01ef424ed12a746eba77e6256306af39c8258875
lukew <luke.wilson@trayport.com>
2014-06-17 00:00:08 +0100

    Music example

--- Neo4jDemo/Program.cs
+++ Neo4jDemo/Program.cs
@@ -13,31 +13,34 @@ namespace Neo4jDemo
          var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
          client.Connect();
 
-         DeleteEntries(client, 17, 18, 19, 20);
+         DeleteEntries(client);
          CreateEntities(client);
          QueryEntities(client);
       }
 
       // ReSharper disable once UnusedMember.Local
-      private static void DeleteEntries(GraphClient client, params int[] ids)
+      private static void DeleteEntries(GraphClient client)
       {
-         foreach (var id in ids)
-            client.Delete(new NodeReference(id), DeleteMode.NodeAndRelationships);
+         var query = client.Cypher.Match("(n)")
+                                  .OptionalMatch("(n)-[r]-()")
+                                  .Delete("n, r");
+         query.ExecuteWithoutResults();
 
-         client.DeleteIndex("idx_People", IndexFor.Node);
+         if (client.CheckIndexExists("idx_artists", IndexFor.Node))
+            client.DeleteIndex("idx_artists", IndexFor.Node);
       }
 
       // ReSharper disable once UnusedMember.Local
       private static void QueryEntities(GraphClient client)
       {
          var query = client.Cypher
-                           .Start(new { n = Node.ByIndexLookup("idx_People", "initials", "RS") })
-                           .Match("n-[r:HATES]->e")
-                           .Return((n, e, r) => new
+                           .Start(new { n = Node.ByIndexLookup("idx_artists", "id", "JH") })
+                           .Match("n-[r1:COVERED]->e")
+                           .Match("e<-[r2:WROTE]-x")
+                           .ReturnDistinct((n, x) => new
                            {
-                              Subject = n.As<Person>().Name,
-                              Object = e.As<Person>().Name,
-                              r.As<HatesData>().Reason
+                              CoveringArtist = n.As<Artist>().Name,
+                              Composer = x.As<Artist>().Name,
                            });
 
          foreach (var result in query.Results)
@@ -49,49 +52,52 @@ namespace Neo4jDemo
       {
          // Create index
          client.CreateIndex(
-            "idx_People",
+            "idx_artists",
             new IndexConfiguration { Provider = IndexProvider.lucene, Type = IndexType.exact },
             IndexFor.Node);
 
          // Create entities
-         var refA = client.Create(new Person { Name = "John" }, null, new[] { new IndexEntry("idx_People") { { "initials", "JL" } } });
-         var refB = client.Create(new Person { Name = "Paul" }, null, new[] { new IndexEntry("idx_People") { { "initials", "PM" } } });
-         var refC = client.Create(new Person { Name = "George" }, null, new[] { new IndexEntry("idx_People") { { "initials", "GH" } } });
-         var refD = client.Create(new Person { Name = "Ringo" }, null, new[] { new IndexEntry("idx_People") { { "initials", "RS" } } });
+         var jimiHendrix = client.Create(new Artist { Name = "Jimi Hendrix" }, null, new[] { new IndexEntry("idx_artists") { { "id", "JH" } } });
+         var bobDylan = client.Create(new Artist { Name = "Bob Dylan" }, null, new[] { new IndexEntry("idx_artists") { { "id", "BD" } } });
+         var theBeetles = client.Create(new Artist { Name = "The Beetles" }, null, new[] { new IndexEntry("idx_artists") { { "id", "TB" } } });
+         var chuckBerry = client.Create(new Artist { Name = "Chuck Berry" }, null, new[] { new IndexEntry("idx_artists") { { "id", "CB" } } });
+
+         var voodooChile = client.Create(new Track {Name = "Voodoo Chile"});
+         var allAlongTheWatchtower = client.Create(new Track {Name = "All Along the Watchtower"});
+         var dayTripper = client.Create(new Track {Name = "Day Tripper"});
+         var sergentPepper = client.Create(new Track {Name = "Sergent Pepper's Lonely Hearts Club Band"});
+         var johnnyBGoode = client.Create(new Track {Name = "Johnny B. Goode"});
 
          // Create relationships
-         client.CreateRelationship(refA, new KnowsRelationship(refB));
-         client.CreateRelationship(refB, new KnowsRelationship(refC));
-         client.CreateRelationship(refB, new HatesRelationship(refD, new HatesData("Crazy guy")));
-         client.CreateRelationship(refC, new HatesRelationship(refD, new HatesData("Don't know why...")));
-         client.CreateRelationship(refD, new KnowsRelationship(refA));
+         client.CreateRelationship(jimiHendrix, new WroteRelationship(voodooChile));
+         client.CreateRelationship(bobDylan, new WroteRelationship(allAlongTheWatchtower));
+         client.CreateRelationship(theBeetles, new WroteRelationship(dayTripper));
+         client.CreateRelationship(theBeetles, new WroteRelationship(sergentPepper));
+         client.CreateRelationship(chuckBerry, new WroteRelationship(johnnyBGoode));
+
+         client.CreateRelationship(jimiHendrix, new CoveredRelationship(allAlongTheWatchtower));
+         client.CreateRelationship(jimiHendrix, new CoveredRelationship(dayTripper));
+         client.CreateRelationship(jimiHendrix, new CoveredRelationship(sergentPepper));
+         client.CreateRelationship(jimiHendrix, new CoveredRelationship(johnnyBGoode));
       }
    }
 
-   public class Person
+   public class Artist
+   {
+      public string Name { get; set; }
+   }
+   
+   public class Track
    {
       public string Name { get; set; }
    }
 
-   public class HatesData
+   public class WroteRelationship :
+      Relationship, IRelationshipAllowingSourceNode<Artist>, IRelationshipAllowingTargetNode<Track>
    {
-      public string Reason { [UsedImplicitly] get; set; }
+      private const string TypeKey = "WROTE";
 
-      public HatesData()
-      { }
-
-      public HatesData(string reason)
-      {
-         Reason = reason;
-      }
-   }
-
-   public class KnowsRelationship :
-      Relationship, IRelationshipAllowingSourceNode<Person>, IRelationshipAllowingTargetNode<Person>
-   {
-      private const string TypeKey = "KNOWS";
-
-      public KnowsRelationship(NodeReference targetNode)
+      public WroteRelationship(NodeReference targetNode)
          : base(targetNode)
       { }
 
@@ -101,13 +107,13 @@ namespace Neo4jDemo
       }
    }
 
-   public class HatesRelationship :
-      Relationship<HatesData>, IRelationshipAllowingSourceNode<Person>, IRelationshipAllowingTargetNode<Person>
+   public class CoveredRelationship :
+      Relationship, IRelationshipAllowingSourceNode<Artist>, IRelationshipAllowingTargetNode<Track>
    {
-      private const string TypeKey = "HATES";
+      private const string TypeKey = "COVERED";
 
-      public HatesRelationship(NodeReference targetNode, HatesData data)
-         : base(targetNode, data)
+      public CoveredRelationship(NodeReference targetNode)
+         : base(targetNode)
       { }
 
       public override string RelationshipTypeKey
